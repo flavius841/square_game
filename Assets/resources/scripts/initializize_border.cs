5ce46235f4d373d1d43ba18a9508df48a5e71ace
flavius841 <flaviusepintilie@gmail.com>
2025-12-14 18:29:37 +0200

    update

---
+++ Assets/resources/scripts/initializize_border.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class initializize_border : MonoBehaviour
+{
+    [SerializeField] GameObject PositionReference;
+
+    void Start()
+    {
+        
+    }
+ 
+    void Update()
+    {
+        
+    }
+
+    // public void InstantiateBorder(GameObject BorderTile, float PositionY, int ID)
+    // {
+    //     GameObject NewSquare = Instantiate(BorderTile);
+    //     NewSquare.transform.position = new Vector3(PositionReference.transform.position.x,
+    //       PositionReference.transform.position.y + PositionY);
+    //     NewSquare.transform.name = ID.ToString();
+    //     NewSquare.transform.SetParent(WholeBorder.transform);
+
+
+
+    // }
+}
