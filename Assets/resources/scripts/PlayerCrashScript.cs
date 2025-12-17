b8ccd457f11149d3f40681c8609e33977297e465
flavius841 <flaviusepintilie@gmail.com>
2025-12-17 15:52:08 +0200

    working on lifes pt 2

--- Assets/resources/scripts/PlayerCrashScript.cs
+++ Assets/resources/scripts/PlayerCrashScript.cs
@@ -1,20 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCrashScript : MonoBehaviour
 {
     [SerializeField] GameObject Parent;
     [SerializeField] bool Crashed;
+    [SerializeField] bool Blink;
     [SerializeField] float DectectingDistance;
     private SpriteRenderer rend;
     [SerializeField] GameObject Life3;
     [SerializeField] GameObject Life2;
     [SerializeField] GameObject Life1;
     [SerializeField] int Lifes = 3;
-    [SerializeField] float BlinkTime;
+    [SerializeField] float Timer;
     [SerializeField] float Speed;
-    [SerializeField] float MinAlpha = 0.5f;
+    [SerializeField] float MinAlpha = 0f;
     [SerializeField] float MaxAlpha = 1f;
-    [SerializeField] bool HasMinAlpha;
+
+
 
 
 
@@ -26,6 +29,10 @@ public class PlayerCrashScript : MonoBehaviour
     void Update()
     {
         PlayerDetector();
+
+        BlinkFunction(Life3, 2);
+        BlinkFunction(Life2, 1);
+        BlinkFunction(Life1, 0);
     }
 
     public void PlayerDetector()
@@ -38,53 +45,62 @@ public class PlayerCrashScript : MonoBehaviour
             {
                 //print(Vector3.Distance(Sphere.transform.position, Player1.transform.GetChild(i).position) + "   " + Player1.transform.GetChild(i).name);
                 if (Vector3.Distance(transform.position, Parent.transform.GetChild(j).GetChild(i).position) < DectectingDistance
-                    && rend.color == Color.red)
+                    && rend.color == Color.red && !Crashed)
                 {
                     Crashed = true;
                     Lifes--;
+                    Blink = true;
                 }
             }
         }
 
     }
 
-    public void LifeCrashAnimation()
-    {
-        // if (lifes == 2 && Crashed)
-        // {
-        //     BlinkTime += Time.deltaTime * Speed;
-        //     if (BlinkTime % 4 == 0)
-        //     {
+    // public void LifeCrashAnimation()
+    // {
+    //     // if (lifes == 2 && Crashed)
+    //     // {
+    //     //     BlinkTime += Time.deltaTime * Speed;
+    //     //     if (BlinkTime % 4 == 0)
+    //     //     {
 
-        //     }
-        // }
+    //     //     }
+    //     // }
 
-        if (Lifes == 2)
-        {
-            rend = Life3.GetComponent<SpriteRenderer>();
-            Color c = rend.color;
-            c.a = MinAlpha;
-            rend.color = c;
-        }
+    //     if (Lifes == 2)
+    //     {
+    //         Timer += Time.deltaTime;
+    //         if (Timer <= 2)
+    //         {
 
-        if (Lifes == 1)
-        {
-            Color c = Life2.color;
-            c.a = MinAlpha;
-            Life2.color = c;
-        }
+    //         }
 
-        if (Lifes == 0)
-        {
-            Color c = Life1.color;
-            c.a = MinAlpha;
-            Life1.color = c;
+    //         rend = Life3.GetComponent<SpriteRenderer>();
+    //         Color c = rend.color;
+    //         c.a = MinAlpha;
+    //         rend.color = c;
+    //     }
 
-            //Invoke("ResetFunction", 0.5f);
-        }
+    //     if (Lifes == 1)
+    //     {
+    //         rend = Life2.GetComponent<SpriteRenderer>();
+    //         Color c = rend.color;
+    //         c.a = MinAlpha;
+    //         rend.color = c;
+    //     }
+
+    //     if (Lifes == 0)
+    //     {
+    //         rend = Life1.GetComponent<SpriteRenderer>();
+    //         Color c = rend.color;
+    //         c.a = MinAlpha;
+    //         rend.color = c;
+
+    //         //Invoke("ResetFunction", 0.5f);
+    //     }
 
 
-    }
+    // }
 
     // public void BlinkFunction(GameObject Life)
     // {
@@ -108,13 +124,36 @@ public class PlayerCrashScript : MonoBehaviour
 
     // }
 
-    public void BlinkFunction(SpriteRenderer sr)
+    public void BlinkFunction(GameObject Life, int NrLifes)
     {
-        float alpha = Mathf.PingPong(Time.time * Speed, MaxAlpha - MinAlpha) + MinAlpha;
+        Image img = Life.GetComponent<Image>();
 
-        Color c = sr.color;
-        c.a = alpha;
-        sr.color = c;
+        if (Lifes == NrLifes && Blink)
+        {
+            Timer += Time.deltaTime;
+            if (Timer <= 2)
+            {
+                float alpha = Mathf.PingPong(Time.time * Speed, MaxAlpha - MinAlpha) + MinAlpha;
+
+                Color c = img.color;
+                c.a = alpha;
+                img.color = c;
+            }
+
+            else
+            {
+                Timer = 0;
+                Blink = false;
+                Crashed = false;
+
+                Color c = img.color;
+                c.a = MinAlpha;
+                img.color = c;
+
+
+            }
+        }
+
     }
 
 }
